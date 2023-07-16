4f843c3ed8f76b5d305c4be1cb27fe8ef8f26357
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-07-16 10:49:16 -0300

    Inicializando a classe "Quarto" através do "rails generate scaffold Quarto numero:integer tipo:string disponibilidade:boolean preco_diaria:decimal descricao:text"

---
+++ db/migrate/20230716134637_create_quartos.rb
@@ -0,0 +1,13 @@
+class CreateQuartos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :quartos do |t|
+      t.integer :numero
+      t.string :tipo
+      t.boolean :disponibilidade
+      t.decimal :preco_diaria
+      t.text :descricao
+
+      t.timestamps
+    end
+  end
+end
