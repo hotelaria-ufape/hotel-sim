7076003e8204d9410d03bd609e3775687ae9cc64
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-07-17 12:52:46 -0300

    Atualizando o formulário de reservas para calcular automaticamente o custo de uma estadia.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :reservas
+  resources :reservas do
+    collection do
+      get 'get_custo'
+    end
+  end
   resources :quartos
   resources :clientes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
