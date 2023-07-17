794ef95beb7bbd9989b453b4efb12b896a2121d3
= <=>
2023-07-17 17:50:13 -0300

    Criado controller de pages e view home, rota de root criada

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   end
   resources :quartos
   resources :clientes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
