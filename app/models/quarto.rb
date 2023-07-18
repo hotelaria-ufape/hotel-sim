b1e611eab7ba4ad427ffc255252c1b847e46713f
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-07-18 11:54:23 -0300

    Atualizando as Validations do "quarto.rb".

--- app/models/quarto.rb
+++ app/models/quarto.rb
@@ -1,12 +1,11 @@
 class Quarto < ApplicationRecord
   has_many :reservas, dependent: :destroy
 
-  validates :numero, presence: true, uniqueness: true, numericality: {only_integer:  true }
-  validates :tipo, presence: true, length: {in: 4..10}
-  validates :disponibilidade, inclusion: { in: [true, false]}
-  validates :preco_diaria, presence: true, numericality: { greater_than: 0 }
-  validates :descricao, presence: true, length: {in: 4..50}
-  validates :preco_diaria, presence: true
+  validates :numero, presence: { message: "não pode estar em branco." }, uniqueness: { message: "de quarto já registrado." }, numericality: {only_integer:  true, message: "deve ter apenas dígitos numéricos." }
+  validates :tipo, presence: { message: "não pode estar em branco." }, inclusion: { in: ['Quarto Padrão', 'Quarto Familiar', 'Quarto de Luxo', 'Suíte Padrão', 'Suíte Presidencial'], message: "deve ser uma opção válida."}
+  validates :disponibilidade, inclusion: { in: [true, false], message: "deve ser uma opção válida."}
+  validates :preco_diaria, presence: { message: "não pode estar em branco." }, numericality: { greater_than: 0, message: "deve ter um valor acima de zero." }
+  validates :descricao, presence: { message: "não pode estar em branco." }, length: {in: 5..256, message: "deve ter entre 5 e 255 caracteres." }
 
   scope :disponibilidade, -> { where(disponibilidade: true) }
 
