b1c019af825cd5ebd259911d87af6b95afa40a1e
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-08-06 08:18:29 -0300

    Atualizando os Testes do Cucumber de "Cliente" para a nova Tradução de Botões e Textos.

--- features/step_definitions/cliente_steps.rb
+++ features/step_definitions/cliente_steps.rb
@@ -8,10 +8,11 @@ When('Eu preencho os dados cpf {string} nome {string} email {string} e telefone:
   fill_in 'cliente[nome]', :with => nome
   fill_in 'cliente[email]', :with => email
   fill_in 'cliente[telefone]', :with => telefone
-  click_button 'Create Cliente'
+  click_button 'Cadastrar Cliente'
 end
 
-Then('Vejo que o meu cadastro foi realizado') do
+Then('Vejo que o {string} foi cadastrado') do |nome|
+  expect(page).to have_content(nome)
   expect(page).to have_content('Cliente criado com sucesso.')
   expect(page).to have_current_path('/clientes/'+Cliente.last.id.to_s)
 end
@@ -22,15 +23,15 @@ Given('O cliente de cpf {string} nome {string} email {string} e telefone: {int} 
   fill_in 'cliente[nome]', :with => nome
   fill_in 'cliente[email]', :with => email
   fill_in 'cliente[telefone]', :with => telefone
-  click_button 'Create Cliente'
+  click_button 'Cadastrar Cliente'
   expect(page).to have_content('Cliente criado com sucesso.')
 end
 
-Then('Eu vejo que meu cpf e invalido') do
+Then('Vejo que seu cpf e invalido') do
   expect(page).to have_content('Cpf informado não é valido.')
 end
 
-Then('Eu vejo que meu email e invalido') do
+Then('Vejo que seu email e invalido') do
   expect(page).to have_content('Email informado não é valido.')
 end
 
@@ -44,17 +45,19 @@ When('Eu clico para remover esse cliente') do
   click_button "Remover cliente"
 end
 
-Then('Eu vejo que o cliente {string} foi corretamente removido') do |nome|
+Then('Eu vejo que o {string} foi corretamente removido') do |nome|
   expect(page).to have_no_content(nome)
+  expect(page).to have_content("Cliente destruído com sucesso")
   expect(page).to have_current_path('/clientes')
 end
 
 When('Eu clico para editar seu telefone para {string}') do |telefone|
   visit '/clientes/'+Cliente.last.id.to_s+'/edit'
   fill_in 'cliente[telefone]', :with => telefone
-  click_button 'Update Cliente'
+  click_button 'Atualizar Cliente'
 end
 
-Then('Eu vejo que o cliente teve seu telefone corretamente alterado para {string}') do |telefone|
+Then('Eu vejo que o {string} teve seu telefone corretamente alterado para {string}') do |nome, telefone|
+  expect(page).to have_content(nome)
   expect(page).to have_content(telefone)
 end
