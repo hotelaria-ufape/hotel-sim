d8162dfe34ababd7f2652a0459f99fd21e62d0a6
= <=>
2023-08-09 07:33:10 -0300

    Novo Sistema de busca de Reservas

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -5,10 +5,14 @@ class ReservasController < ApplicationController
   def index
     @reservas = Reserva.all
 
-    if params[:start_date].present? && params[:end_date].present?
+    if params[:attribute] == "date" && params[:start_date].present? && params[:end_date].present?
       start_date = DateTime.parse(params[:start_date])
       end_date = DateTime.parse(params[:end_date])
       @reservas = @reservas.where("(data_de_entrada BETWEEN ? AND ?) OR (data_de_saida BETWEEN ? AND ?)", start_date, end_date, start_date, end_date)
+    elsif params[:attribute] == "cost" && params[:min_cost].present? && params[:max_cost].present?
+      min_cost = params[:min_cost].to_f
+      max_cost = params[:max_cost].to_f
+      @reservas = @reservas.where(custo: min_cost..max_cost)
     end
   end
 
