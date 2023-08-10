ccf1dd01ddc648d370127b0351de3b80d834d94d
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-08-10 14:44:55 -0300

    Implementação de Busca de Quartos (Instável).

--- app/controllers/quartos_controller.rb
+++ app/controllers/quartos_controller.rb
@@ -3,16 +3,34 @@ class QuartosController < ApplicationController
 
   # GET /quartos or /quartos.json
   def index
-    if params[:disponibilidade].present?
-      @quartos = Quarto.where(disponibilidade: params[:disponibilidade])
-    else
-      @quartos = Quarto.all
+    @quartos = Quarto.all
+
+    if params[:attribute].present? && params[:search].present?
+      attribute = params[:attribute]
+      search_query = params[:search]
+
+      case attribute
+      when "numero"
+        @quartos = @quartos.where('numero LIKE ?', "%#{search_query}%")
+      when "tipo"
+        @quartos = @quartos.where('tipo LIKE ?', "%#{search_query}%")
+      when "disponibilidade"
+        @quartos = @quartos.where(disponibilidade: search_query)
+      when "preco_diaria"
+        @quartos = @quartos.where('preco_diaria LIKE ?', "%#{search_query}%")
+      when "descricao"
+        @quartos = @quartos.where('descricao LIKE ?', "%#{search_query}%")
+      when "quantidade_de_hospedes"
+        @quartos = @quartos.where('quantidade_de_hospedes = ?', search_query)
+      end
     end
-    @quartos = @quartos.where('preco_diaria BETWEEN ? AND ?', params[:min_price], params[:max_price]) if params[:max_price].present? and params[:min_price].present?
   end
 
   # GET /quartos/1 or /quartos/1.json
   def show
+    @quarto = Quarto.find(params[:id])
+    @reservas_passadas = @quarto.reservas.where('data_de_saida < ?', Time.zone.today)
+    @reservas_futuras = @quarto.reservas.where('data_de_entrada > ?', Time.zone.today)
   end
 
   # GET /quartos/new
@@ -69,13 +87,13 @@ class QuartosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_quarto
-      @quarto = Quarto.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quarto
+    @quarto = Quarto.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def quarto_params
-      params.require(:quarto).permit(:numero, :tipo, :disponibilidade, :preco_diaria, :descricao, :quantidade_de_hospedes)
-    end
+  # Only allow a list of trusted parameters through.
+  def quarto_params
+    params.require(:quarto).permit(:numero, :tipo, :disponibilidade, :preco_diaria, :descricao, :quantidade_de_hospedes)
+  end
 end
